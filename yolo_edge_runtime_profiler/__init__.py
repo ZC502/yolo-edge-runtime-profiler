d7b32ba385bfcf63de5c3718bad30794a192f9c3
ZuoCen Liu <liouzuocen@qq.com>
2026-07-04 11:59:21 +0800

    Add LocalHardExampleRecorder and update version
    Added LocalHardExampleRecorder and related classes to __all__ and updated version.

--- yolo_edge_runtime_profiler/__init__.py
+++ yolo_edge_runtime_profiler/__init__.py
@@ -1,11 +1,15 @@
 from .profiler import YoloEdgeRuntimeProfiler, ProfilerConfig
 from .status import RuntimeStatus, RuntimeState
+from .hard_examples import LocalHardExampleRecorder, LocalHardExampleConfig, CaptureResult
 
 __all__ = [
     "YoloEdgeRuntimeProfiler",
     "ProfilerConfig",
     "RuntimeStatus",
     "RuntimeState",
+    "LocalHardExampleRecorder",
+    "LocalHardExampleConfig",
+    "CaptureResult",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
